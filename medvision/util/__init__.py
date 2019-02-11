3549339316538a2cf22cc79cda2ebceb2b7b15e1
kyle0x54 <kyle0x54@163.com>
2019-02-12 00:49:23 +0800

    Fix GlobMode missing bug.

--- medvision/util/__init__.py
+++ medvision/util/__init__.py
@@ -1,8 +1,10 @@
 from .fileutil import (isdir, isfile, joinpath, basename, cp, rm,
-                       cptree, rmtree, move, mkdirs, copyfiles, glob)
+                       cptree, rmtree, move, mkdirs, copyfiles,
+                       GlobMode, glob)
 
 
 __all__ = [
     'isdir', 'isfile', 'joinpath', 'basename', 'cp', 'rm',
-    'cptree', 'rmtree', 'move', 'mkdirs', 'copyfiles', 'glob'
+    'cptree', 'rmtree', 'move', 'mkdirs', 'copyfiles',
+    'GlobMode', 'glob'
 ]
