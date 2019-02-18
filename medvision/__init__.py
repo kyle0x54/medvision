cad8a0d5f942649542e2d6331099ec9a537d7249
kyle0x54 <kyle0x54@163.com>
2019-02-19 00:28:38 +0800

    Relocate dataset utility functions to dataset module.

--- medvision/__init__.py
+++ medvision/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from .__version__ import __version__
+from .dataset import *
 from .dicom import *
+from .image import *
 from .util import *
 from .visualization import *
-from .image import *
---
+++ medvision/dataset/__init__.py
@@ -0,0 +1,2 @@
+from .util import (load_dsmd, save_dsmd, gen_cls_dsmd_file_from_datafolder,
+                   gen_cls_ds_from_datafolder, split_dsmd_file)
