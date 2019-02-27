fix: fix gray scale image normalization bug.

medvision/image/transforms/normalize.py
@@ -2,7 +2,7 @@ import numpy as np
 import medvision as mv
 
 
-def normalize_grayscale(src, to_float=True, epsilon=1e-7):
+def normalize_grayscale(src, to_float=True):
     """ Rescale image intensity.
 
     Rescale an grayscale image's intensity range to [0.0, 1.0].
@@ -11,27 +11,27 @@ def normalize_grayscale(src, to_float=True, epsilon=1e-7):
         src (ndarray): image to be intensity rescaled.
         to_float (bool): whether to convert the input image to float
             before intensity rescale.
-        epsilon: a regularization term to avoid divide by 0 error.
 
     Return:
         (ndarray): intensity rescaled image.
     """
     assert src.ndim == 2
+    epsilon = np.finfo(np.float32).eps
 
     if to_float:
-        img = src.astype(np.float32)
+        src = src.astype(np.float32)
 
-    min_val, max_val = np.min(img), np.max(img)
+    min_val, max_val = np.min(src), np.max(src)
     if max_val - min_val < epsilon:
         max_val += epsilon
 
-    return (img - min_val) / (max_val - min_val)
+    return (src - min_val) / (max_val - min_val)
 
 
 def normalize_rgb(img, mean, std):
     """ Normalize an image.
 
-    Dubtract mean per channel and divide by std per channel. (support
+    Subtract mean per channel and divide by std per channel. (support
     grayscale image and RGB image).
 
     Args: