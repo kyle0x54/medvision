f5ef41582c223f88abf0804fb389a6ef78c0cfa1
liupengfei <liupengfei@huiyihuiying.com>
2019-02-28 17:20:46 +0800

    fix: expose load&save detection dsmd api

--- medvision/dataset/__init__.py
+++ medvision/dataset/__init__.py
@@ -1,8 +1,12 @@
 from .util import (load_dsmd, save_dsmd, gen_cls_dsmd_file_from_datafolder,
                    gen_cls_ds_from_datafolder, split_dsmd_file)
 
+from .util import load_dsmd_det, save_dsmd_det
+
 
 __all__ = [
     'load_dsmd', 'save_dsmd', 'gen_cls_dsmd_file_from_datafolder',
-    'gen_cls_ds_from_datafolder', 'split_dsmd_file'
+    'gen_cls_ds_from_datafolder', 'split_dsmd_file',
+
+    'load_dsmd_det', 'save_dsmd_det'
 ]
