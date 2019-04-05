e80dca5a0ceac1c6e5ecc3d600c101fb71209726
kyle0x54 <kyle0x54@163.com>
2019-04-05 23:55:30 +0800

    feat: add roc and pr curve drawing utility functions

--- medvision/visualization/__init__.py
+++ medvision/visualization/__init__.py
@@ -1,10 +1,10 @@
-from .draw_curve import draw_froc
+from .draw_curve import draw_froc, save_roc_curve, save_pr_curve
 from .image import Color, imshow, imshow_bboxes
 from .visdom_visualizer import VisdomVisualizer
 from .tensorboard_visualizer import TensorboardVisualizer
 
 __all__ = [
-    'draw_froc',
+    'draw_froc', 'save_roc_curve', 'save_pr_curve',
     'Color', 'imshow', 'imshow_bboxes',
     'VisdomVisualizer',
     'TensorboardVisualizer'
--- medvision/visualization/draw_curve.py
+++ medvision/visualization/draw_curve.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import sklearn
 
 
 def draw_froc(average_fps, sensitivity, save_path=None, **kwargs):
@@ -18,6 +19,41 @@ def draw_froc(average_fps, sensitivity, save_path=None, **kwargs):
     plt.ylim([0, 1])
     plt.plot(average_fps, sensitivity, **kwargs)
     plt.grid()
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def save_roc_curve(roc_curve, save_path=None):
+    fpr, tpr, _ = roc_curve
+    roc_auc = sklearn.metrics.auc(fpr, tpr)
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2,
+             label='ROC curve (auc = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.xticks()
+    plt.yticks()
+    plt.title('ROC Curve')
+    plt.legend(loc="lower right")
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def save_pr_curve(pr_curve, save_path=None):
+    precision, recall, _ = pr_curve
+    plt.plot(recall, precision, color='darkorange', lw=2)
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.xticks()
+    plt.yticks()
+    plt.title('PR Curve')
     if save_path is not None:
         plt.savefig(save_path)
     plt.show()
