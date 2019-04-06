b8ac7cb61287aec58eba24c08b8d099566a35b4a
kyle0x54 <kyle0x54@163.com>
2019-04-07 01:53:26 +0800

    feat: add AverageMeter utility class

--- medvision/torch_util/__init__.py
+++ medvision/torch_util/__init__.py
@@ -1,12 +1,12 @@
 from .checkpoint import (save_checkpoint, load_checkpoint,
                          save_ckpt_to_dir, load_ckpt_from_dir)
 from .dataset.cls_dataloader import build_cls_dataloader
-from .util import ModeKey, nograd, make_np
+from .util import ModeKey, nograd, make_np, AverageMeter
 
 
 __all__ = [
     'save_checkpoint', 'load_checkpoint',
     'save_ckpt_to_dir', 'load_ckpt_from_dir',
     'build_cls_dataloader',
-    'ModeKey', 'nograd', 'make_np'
+    'ModeKey', 'nograd', 'make_np', 'AverageMeter'
 ]
