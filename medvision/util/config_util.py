2cbd0596a5deedbdc49bc8b6a60c382d466f0d2e
kyle0x54 <kyle0x54@163.com>
2019-04-06 17:30:18 +0800

    feat: support secondary configurations auto-change according to primary configurations

--- medvision/util/config_util.py
+++ medvision/util/config_util.py
@@ -1,16 +1,22 @@
 import argparse
 
 
-def load_config_cmdline(load_default, description='', log_cfg=False):
-    """ Load configuration from command line.
+def load_config_cmdline(load_default_config,
+                        update_config,
+                        description='',
+                        log_cfg=False):
+    """ Load configurations from command line.
 
     Args:
-        load_default (yacs CfgNode): function to load default configuration.
+        load_default_config (function): function to load default
+            configurations.
+        update_config (function): function to update secondary
+            configurations according to primary configurations.
         description(str): refer to argparse.ArgumentParser description.
-        log_cfg(bool): whether or not to log configuration.
+        log_cfg(bool): whether or not to log configurations.
 
     Return:
-        (yacs CfgNode): loaded configuration
+        (yacs CfgNode): loaded configurations
 
     Example:
         <<< python train.py --config_file config.yaml
@@ -31,7 +37,7 @@ def load_config_cmdline(load_default, description='', log_cfg=False):
     args = parser.parse_args()
 
     # 2 load default configuration
-    cfg = load_default()
+    cfg = load_default_config()
 
     # 3 load configuration from command line
     if args.config_file is not None:
@@ -40,28 +46,39 @@ def load_config_cmdline(load_default, description='', log_cfg=False):
     if args.opts is not None:
         cfg.merge_from_list(args.opts)
 
+    # 4. update configurations if primary configuration changes
+    update_config(cfg)
+
     cfg.freeze()
 
-    # 4. log configuration for visual check
+    # 5. log configuration for visual check
     if log_cfg:
         print('Running with config:\n{}'.format(cfg))
 
     return cfg
 
 
-def load_config(load_default, config_file_path=None):
+def load_config(load_default_config,
+                update_config,
+                config_file_path=None):
     """ Load configuration.
 
     Args:
-        load_default (yacs CfgNode): function to load default configuration.
+        load_default_config (function): function to load default
+            configurations.
+        update_config (function): function to update secondary
+            configurations according to primary configurations.
         config_file_path (str): configuration file path.
 
     Return:
-        (yacs CfgNode): loaded configuration
+        (yacs CfgNode): loaded configurations
     """
     # TODO: add unit test
-    cfg = load_default()
+    cfg = load_default_config()
+
     if config_file_path is not None:
         cfg.merge_from_file(config_file_path)
+    update_config(cfg)
+
     cfg.freeze()
     return cfg
