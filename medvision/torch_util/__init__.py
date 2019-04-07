b1954ceaeb42d9559b215ceb1bd44dcd55e14e95
kyle0x54 <kyle0x54@163.com>
2019-04-08 00:55:41 +0800

    revert: remove AverageMeter

--- medvision/torch_util/__init__.py
+++ medvision/torch_util/__init__.py
@@ -1,12 +1,12 @@
 from .checkpoint import (save_checkpoint, load_checkpoint,
                          save_ckpt_to_dir, load_ckpt_from_dir)
 from .dataset.cls_dataloader import build_cls_dataloader
-from .util import ModeKey, nograd, make_np, AverageMeter
+from .util import ModeKey, nograd, make_np
 
 
 __all__ = [
     'save_checkpoint', 'load_checkpoint',
     'save_ckpt_to_dir', 'load_ckpt_from_dir',
     'build_cls_dataloader',
-    'ModeKey', 'nograd', 'make_np', 'AverageMeter'
+    'ModeKey', 'nograd', 'make_np'
 ]
