81c4b932368bd3a41c334801d54c042def42da39
liupengfei <liupengfei@huiyihuiying.com>
2019-04-09 21:19:40 +0800

    feat: initial version of bdc annotation loading and conversion

---
+++ medvision/annotation/bdc2dsmd.py
@@ -0,0 +1,49 @@
+import numpy as np
+from .load_annotation import load_bdc_dr_bbox
+import medvision as mv
+
+
+def bdc2dsmd_det_2d(annot_dir,
+                    image_dir=None,
+                    class2label=None,
+                    ignore_label_name=True,
+                    replace_ext=lambda x: x):
+    # N.B. annotation file name and image file name should be the same
+    num_classes = len(class2label) if class2label is not None else 1
+
+    filenames = mv.listdir(annot_dir)
+    empty_bboxes = np.zeros((0, 4), dtype=np.float32)
+    dsmd = {replace_ext(filename): [empty_bboxes] * num_classes
+            for filename in filenames}
+    for filename in filenames:
+        annot_filepath = mv.joinpath(annot_dir, filename)
+        bboxes = load_bdc_dr_bbox(
+            annot_filepath,
+            lambda x: 0 if ignore_label_name else class2label
+        )
+        for label, bbox in bboxes:
+            bbox = np.array(bbox, dtype=np.float32).reshape(-1, 4)
+            if dsmd[replace_ext(filename)][label].shape[0] == 0:
+                dsmd[replace_ext(filename)][label] = bbox
+            else:
+                dsmd[replace_ext(filename)][label] = np.append(
+                    dsmd[replace_ext(filename)][label], bbox, axis=0)
+
+    return mv.make_dsmd(dsmd)
+
+
+if __name__ == '__main__':
+    # TODO: move to unittest
+    annot_dir = '/mnt/sdb1/tb/internal/label'
+    dsmd_path = '/mnt/sdb1/tb/internal/1.csv'
+    replace_ext = lambda x: x.replace('.txt', '.dcm')
+    dsmd = bdc2dsmd_det_2d(annot_dir, replace_ext=replace_ext)
+    mv.save_dsmd(dsmd_path, dsmd, {'tb': 0}, mode='det')
+
+    dsmd = mv.load_dsmd(dsmd_path, {'tb': 0}, mode='det')
+    bboxes = dsmd['002565.dcm'][0]
+    image_path = '/mnt/sdb1/tb/internal/tb/002565.dcm'
+    image = mv.dcmread_dr(image_path)
+    image = mv.normalize_grayscale(image) * 255.0
+    image = image.astype(np.uint8)
+    mv.imshow_bboxes(image, bboxes, thickness=4)
---
+++ medvision/annotation/load_annotation.py
@@ -0,0 +1,77 @@
+import json
+import cv2
+import numpy as np
+
+
+def load_bdc_dr_annot(annot_path, class2label=None):
+    """ Load a DR BDC annotation file.
+
+    Args:
+        annot_path (str): annotation file path.
+        class2label (dict or callable): convert classname to label. If
+            it is not dict or callable, the classname will not be converted.
+
+    Returns:
+        (list[ndarray]): a list containing all annotation contours.
+    """
+    with open(annot_path, 'rt') as fd:
+        lines = fd.readlines()
+
+    contours = []
+    for line in lines:
+        classname, content = line.split('|')
+
+        if isinstance(class2label, dict):
+            label = class2label[classname]
+        elif callable(class2label):
+            label = class2label(classname)
+        else:
+            label = classname
+
+        contour = np.array(json.loads(content)).astype(np.int)
+        contour = contour[:, :2]
+        contour = np.reshape(contour, (-1, 1, 2))
+        contours.append((label, contour))
+
+    return contours
+
+
+def _contour2bbox(contour):
+    x, y, w, h = cv2.boundingRect(contour)
+    return [x, y, x + w, y + h]
+
+
+def load_bdc_dr_bbox(annot_path, class2label=None):
+    """ Load a 2D BDC annotation file and convert it to a bounding box.
+
+    Args:
+        annot_path (str): annotation file path.
+        class2label (dict or callable): convert classname to label. If
+            it is not dict or callable, the classname will not be converted.
+
+    Returns:
+        (list[ndarray]): a list containing converted annotation bboxes.
+    """
+    bboxes = []
+
+    contours = load_bdc_dr_annot(annot_path, class2label)
+    for label, contour in contours:
+        bbox = _contour2bbox(contour)
+        bboxes.append((label, bbox))
+
+    return bboxes
+
+
+if __name__ == '__main__':
+    # TODO: move to unittest
+    image_path = '/mnt/sdb1/tb/internal/tb/002565.dcm'
+    annot_path = '/mnt/sdb1/tb/internal/label/002565.txt'
+    bboxes = load_bdc_dr_bbox(annot_path, lambda x: 1)
+    import medvision as mv
+    image = mv.dcmread_dr(image_path)
+    image = mv.normalize_grayscale(image) * 255.0
+    image = image.astype(np.uint8)
+    tmp = [bbox for label, bbox in bboxes]
+    tmp = np.array(tmp)
+    mv.imshow_bboxes(image, tmp, thickness=4)
+    print(bboxes)
