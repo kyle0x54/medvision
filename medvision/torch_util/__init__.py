a5610fc10397bb6f58c02d2cab2bd22f61f4b59e
liupengfei <liupengfei@huiyihuiying.com>
2019-05-07 17:03:40 +0800

    feat: expose Runner and Hook API to user

--- medvision/torch_util/__init__.py
+++ medvision/torch_util/__init__.py
@@ -1,6 +1,7 @@
 from .checkpoint import (save_checkpoint, load_checkpoint,
                          save_ckpt_to_dir, load_ckpt_from_dir)
 from .dataset.cls_dataloader import build_cls_dataloader
+from .runner import Hook, LoggerHook, ClsLoggerHook, Runner
 from .util import ModeKey, nograd, make_np
 
 
@@ -8,5 +9,6 @@ __all__ = [
     'save_checkpoint', 'load_checkpoint',
     'save_ckpt_to_dir', 'load_ckpt_from_dir',
     'build_cls_dataloader',
+    'Hook', 'LoggerHook', 'ClsLoggerHook', 'Runner',
     'ModeKey', 'nograd', 'make_np'
 ]
