823cd6bb7ff4a62ac0a46cc562bba80a21c03db3
liupengfei <liupengfei@huiyihuiying.com>
2019-07-18 18:23:32 +0800

    feat: support converting mask annotation to rws format

--- medvision/annotation/__init__.py
+++ medvision/annotation/__init__.py
@@ -1,6 +1,10 @@
 from .load_annotation import load_bdc_dr_contour, load_bdc_dr_bbox
+from .bdc2rws import batch_bdc2rws_contour
+from .mask2rws import batch_mask2rws_contour
 
 __all__ = [
     'load_bdc_dr_contour',
     'load_bdc_dr_bbox',
+    'batch_bdc2rws_contour',
+    'batch_mask2rws_contour',
 ]
