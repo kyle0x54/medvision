80b93134289ffb899f9841e14a6b5c79f78e6279
kyle0x54 <kyle0x54@163.com>
2019-12-13 18:15:54 +0800

    refactor: remove unneeded bdc utility functions

--- medvision/annotation/__init__.py
+++ medvision/annotation/__init__.py
@@ -1,5 +1,3 @@
-from .bdc import load_bdc_dr_contour, load_bdc_dr_bbox
-from .bdc2rws import batch_bdc2rws_contour
 from .mask2rws import batch_mask2rws, mask2rws
 from .rws2mask import batch_rws2mask, rws2mask
 from .rws2dsmd import rws2dsmd_bbox
@@ -14,9 +12,6 @@ from .rws import (
 
 
 __all__ = [
-    'load_bdc_dr_contour',
-    'load_bdc_dr_bbox',
-    'batch_bdc2rws_contour',
     'batch_mask2rws', 'mask2rws',
     'batch_rws2mask', 'rws2mask', 'rws2dsmd_bbox',
     'get_rws_datainfo_path', 'get_rws_annot_path',
