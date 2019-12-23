6e1bd7aab0b9ee2b10082dea3d05c8a30095ea96
kyle0x54 <kyle0x54@163.com>
2019-12-23 21:14:36 +0800

    feat: add load_rws_bbox()
    1. add load_rws_bbox function
    2. refactor rws2dsmd

--- medvision/annotation/rws.py
+++ medvision/annotation/rws.py
@@ -63,6 +63,93 @@ def load_rws_bbox(filepath):
     return rws_bboxes
 
 
+def gen_rws_shape_bbox(bbox, label):
+    flags = {}
+    shape_type = 'rectangle'
+    return dict(
+        label=label,
+        points=[[float(bbox[0]), float(bbox[1])],
+                [float(bbox[2]), float(bbox[3])]],
+        shape_type=shape_type,
+        flags=flags
+    )
+
+
+def save_rws_bbox(
+    filepath,
+    shapes,
+    image_shape,
+    suffix=".json",
+    score_thresh=0,
+    label_="auto"
+):
+    """
+    Save bounding boxes into rws file.
+
+    Args:
+        filepath (str): rws file path.
+        shapes (list or numpy.ndarray): "shape" field in rws file or
+            raw bounding boxes (in numpy.ndarray format or list format,
+            in this case, only single-class label is supported).
+        image_shape (tuple): shape of corresponding image, in (h, w) format.
+        suffix (str): rws file suffix, e.g. ".json", ".json_EA", ".json_A1".
+        score_thresh (float): probability threshold of bounding boxes.
+            If probability score of a box is larger than the threshold,
+            the box will be saved into rws file, otherwise not.
+        label_ (str): bounding box label (for single class object detection).
+    """
+    assert len(shapes) != 0, "no bounding box for [%s]" % filepath
+    assert suffix.startswith(".json"), "unsupported suffix %s" % suffix
+
+    # if not default rws suffix, change suffix
+    if suffix != ".json":
+        filepath = mv.joinpath(
+            mv.parentdir(filepath),
+            mv.filetitle(filepath) + suffix
+        )
+
+    # for raw bounding boxes, convert to rws "shape" field format first.
+    bboxes = shapes
+    shapes = []
+    for bbox in bboxes:
+        if len(bbox) == 2:  # rws "shape" field format
+            label, bbox = bbox
+        elif len(bbox) == 5 and bbox[-1] < score_thresh:
+            continue
+        else:  # ndarray or box list
+            label, bbox = label_, bbox[:4]
+        shape = gen_rws_shape_bbox(bbox, label)
+        shapes.append(shape)
+
+    # establish rws file content
+    data = dict(
+        version='0.1.0',
+        flags={},
+        shapes=shapes,
+        lineColor=None,
+        fillColor=None,
+        imagePath=mv.filetitle(filepath) + ".dcm",
+        imageData=None,
+        imageHeight=image_shape[0],
+        imageWidth=image_shape[1],
+    )
+
+    # save rws file
+    with open(filepath, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
 if __name__ == '__main__':
-    bboxes = load_rws_bbox('/home/huiying/Test/test.json')
+    rws_filepath = '0.json'
+    bboxes = load_rws_bbox(rws_filepath)
     print(bboxes)
+    save_rws_bbox(
+        mv.joinpath(
+            mv.parentdir(rws_filepath),
+            mv.filetitle(rws_filepath) + '.json_A1'
+        ),
+        bboxes['shapes'],
+        (2947, 3000),
+        suffix='.json_A1',
+        label_='saved_box'
+    )
--- medvision/annotation/rws2dsmd.py
+++ medvision/annotation/rws2dsmd.py
@@ -1,66 +1,68 @@
-import json
 import numpy as np
 import medvision as mv
 
 
-def rws2dsmd_bbox(filepaths, num_classes, class2label):
+def rws2dsmd_bbox(
+    filepaths,
+    class2label=None,
+    suffix='.json',
+    num_classes=None
+):
     """ Convert rws bbox annotation to dsmd.
 
     Args:
         filepaths (str or list): file paths of rws annotation files or
             directory containing rws annotation files.
-        num_classes (int): number of classes.
-        class2label (str or callable): class-to-label file or class2label dict.
-
+        class2label (str, dict or callable): class-to-label file,
+            class2label dict or a callable.
+        suffix (str): suffix of rws file. e.g. ".json", ".json_A1"
+        num_classes (int or None): if class2label is a callable, num_classes
+            should be explicitly specified.
     N.B.
         dsmd key value is file title.
     """
-    if isinstance(class2label, str):
-        class2label = mv.load_c2l(class2label)
+    if callable(class2label):
+        assert num_classes is not None,\
+            'must specify num_classes if class2label is a callable'
+    else:
+        if isinstance(class2label, str):
+            class2label = mv.load_c2l(class2label)
+        num_classes = len(class2label)
 
     if isinstance(filepaths, str):
-        filepaths = mv.glob(filepaths, '*.json')
+        filepaths = mv.glob(filepaths, '*' + suffix)
 
     dsmd = {}
     for filepath in filepaths:
         key = mv.filetitle(filepath)
-        dsmd[key] = [None] * num_classes
         # to handle cases without annot when getting annot path from image path
         if not mv.isfile(filepath):
             continue
         shapes = mv.load_rws_bbox(filepath)['shapes']
-        for label, bbox in shapes:
-            label = class2label(label)
-            bbox = np.array(bbox)
-            if dsmd[key][label] is None:
-                dsmd[key][label] = [bbox]
-            else:
-                dsmd[key][label].append(bbox)
-    dsmd = mv.convert_bboxes_format(dsmd)
-    dsmd = mv.make_dsmd(dsmd)
-    return dsmd
+        dsmd[key] = [np.zeros((0, 4), dtype=np.float32)] * num_classes
+        for classname, box in shapes:
+            try:
+                label = class2label[classname]
+            except TypeError:
+                label = class2label(classname)
+            dsmd[key][label] = np.append(dsmd[key][label], [box], axis=0)
+    return mv.make_dsmd(dsmd)
 
 
-def _gen_rws_shape(bbox, label):
-    flags = {}
-    shape_type = 'rectangle'
-    return dict(
-        label=label,
-        points=[[float(bbox[0]), float(bbox[1])],
-                [float(bbox[2]), float(bbox[3])]],
-        shape_type=shape_type,
-        flags=flags
-    )
-
-
-def dsmd2rws_det(dsmd, dcm_dir, suffix='.json_AI', class2label=None, thr=0.3):
+def dsmd2rws_bbox(
+    dsmd,
+    dcm_dir,
+    class2label=None,
+    suffix='.json_A1',
+    score_thresh=0.3
+):
     """ Convert dsmd to rws bbox annotations.
     Args:
         dsmd (str or dsmd): file path of dsmd file or dsmd data.
         dcm_dir (str): dicom directory.
-        suffix (str): suffix of output rws file, For example, ".json_EA".
         class2label (str or dict): class-to-label file or class2label dict.
-        thr (float): threshold of confidence.
+        suffix (str): suffix of output rws file, For example, ".json_EA".
+        score_thresh (float): threshold of confidence.
     N.B.
         dsmd key value is file title.
     """
@@ -71,41 +73,31 @@ def dsmd2rws_det(dsmd, dcm_dir, suffix='.json_AI', class2label=None, thr=0.3):
     if isinstance(dsmd, str):
         dsmd = mv.load_dsmd(dsmd, class2label, mode='det')
 
-    dcm_files = mv.glob(dcm_dir, "*.dcm")
-    assert len(dcm_files) == len(dsmd)
-    dcm_infos = {}
-    for dcm_file in dcm_files:
-        ds = mv.dcminfo(dcm_file)
-        dcm_infos[mv.filetitle(dcm_file)] = [dcm_file, ds.Rows, ds.Columns]
+    dcm_paths = mv.glob(dcm_dir, "*.dcm")
+    assert len(dcm_paths) == len(dsmd)
 
     for key in dsmd.keys():
         shapes = []
         for i, bboxes in enumerate(dsmd[key]):
             for bbox in bboxes:
-                if len(bbox) == 5 and bbox[-1] < thr:
+                if len(bbox) == 5 and bbox[-1] < score_thresh:
                     continue
                 label = label2class[i] if len(bbox) == 4 else \
                     label2class[i] + "_{:.3f}".format(bbox[-1])
-                shape = _gen_rws_shape(bbox[:4], label)
-                shapes.append(shape)
+                shapes.append((label, bbox[:4].tolist()))
         if len(shapes) == 0:
             continue
-        data = dict(
-            version='0.1.0',
-            flags={},
-            shapes=shapes,
-            lineColor=None,
-            fillColor=None,
-            imagePath=mv.basename(dcm_infos[key][0]),
-            imageData=None,
-            imageHeight=dcm_infos[key][1],
-            imageWidth=dcm_infos[key][2],
-        )
+
         rws_path = mv.joinpath(dcm_dir, key + suffix)
-        with open(rws_path, 'w') as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
+        ds = mv.dcminfo(mv.joinpath(dcm_dir, key + '.dcm'))
+        mv.save_rws_bbox(rws_path, shapes, (ds.Rows, ds.Columns))
 
 
 if __name__ == '__main__':
-    dsmd = rws2dsmd_bbox(['/home/huiying/Test/test.json'], 1, lambda x: 0)
+    dsmd = rws2dsmd_bbox(
+        ['0.json'],
+        lambda x: 0,
+        num_classes=1
+    )
+    dsmd2rws_bbox(dsmd, "dcm_dir", {"classname": 0})
     print(dsmd)
